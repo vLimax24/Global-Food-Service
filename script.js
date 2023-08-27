7ffb512e4790b36a2b54ed0fb38a7a8ae4d2f0c0
Limax <limax.workspace@gmail.com>
2023-08-27 20:21:53 +0200

    bug fixes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const handleOnMove = e => {
   
   const percentage = (mouseDelta / maxDelta) * -100,
         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -60);
   
   track.dataset.percentage = nextPercentage;
   
